1c81987a939e10abceeb2ccf631e612bf1744efa
Kylee-Fish <76824475+Kylee-Fish@users.noreply.github.com>
2021-04-05 21:56:15 -0500

    Added forecast for 6 future days

--- src/weather.js
+++ src/weather.js
@@ -38,8 +38,9 @@ let currentTime = document.querySelector("h2");
 currentTime.innerHTML = `${day}, ${month} ${date}, ${year} ${hour}:${minute}`;
 
 
+
 function search(city) {
-  let apiKey = "b72128b99d2ac3a4ff4a2be1ec63d412";
+  let apiKey = `b72128b99d2ac3a4ff4a2be1ec63d412`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showWeather);
 }
@@ -48,7 +49,7 @@ function submitCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   search(city);
-
+  
 }
 
 function showWeather(response) {
@@ -63,7 +64,7 @@ function showWeather(response) {
   `Humidity: ${Math.round(response.data.main.humidity)}%`;
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].main);
-
+  getForecast(response.data.coord);
 }
 
 function showCelsius(event) {
@@ -87,34 +88,59 @@ function showFahrenheit(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = `b72128b99d2ac3a4ff4a2be1ec63d412`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let weekdays = ["Thu", "Fri", "Sat", "Sun"];
-  weekdays.forEach(function (day) {
-    forecastHTML = forecastHTML + `
-        <div class="col-2">
-          <div class="weather-forecast-day">
-            ${day}
-          </div>
-            <img 
-            src="http://openweathermap.org/img/wn/04d@2x.png" 
-            alt="clouds" 
-            class="forecast-image" 
-            />
-          <div class="weather-forecast-temperatures">
-            <span class="max-temp">
-              78°
-            </span>
-            <span class="min-temp">
-              56°
-            </span>
-          </div>
-        </div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index >= 1 && index <= 6) {
+    forecastHTML = forecastHTML + 
     `
-  })
+    <div class="col-2">
+    <div class="weather-forecast-day">
+    ${formatDay(forecastDay.dt)}
+    </div>
+    ${index}
+    <img 
+    src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+    alt="clouds" 
+    class="forecast-image" 
+    />
+    <div class="weather-forecast-temperatures">
+    <span class="max-temp">
+    ${Math.round(forecastDay.temp.max)}
+    </span>
+    <span class="min-temp">
+    ${Math.round(forecastDay.temp.min)}
+    </span>
+    </div>
+    </div>
+    `;
+    }
+  });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function formatDay(timestamp) {
+  let dayNumber = new Date(timestamp * 1000);
+  let weekday = dayNumber.getDay();
+  let weekdays = [
+  "Sun", 
+  "Mon", 
+  "Tue", 
+  "Wed", 
+  "Thu", 
+  "Fri", 
+  "Sat"
+];
+  return weekdays[weekday];
 }
 
 let fahrenheitTemperature = null;
@@ -129,4 +155,3 @@ let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", showFahrenheit);
 
 search("Austin");
-displayForecast();
